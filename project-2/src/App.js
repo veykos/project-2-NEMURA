8eafc906b47e06dad856f4a3bff49c0e1809d0d6
mulybirish <mulugata13@gmail.com>
2022-10-26 09:38:22 +0200

    initial init-2

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Wellcome to project #2</h1>
+      <iframe
+        src="https://giphy.com/embed/l0MYC0LajbaPoEADu"
+        width="480"
+        height="277"
+        frameBorder="0"
+        class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <p>
+        <a href="https://giphy.com/gifs/welcome-austin-powers-dr-evil-l0MYC0LajbaPoEADu">
+          via GIPHY
         </a>
-      </header>
+      </p>
     </div>
   );
 }
