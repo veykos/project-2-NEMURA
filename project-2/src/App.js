c7aae4600ee5a47199fb00c6667d8618201f43f1
Nevin Alisheva <nevin.alisheva@gmail.com>
2022-10-27 19:55:32 +0200

    navbar done

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import {Navbar} from './components/UI/navbar.jsx';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+//import {Home} from "./pages/home"; home tusunu kullanabilmek icin 
+//import {Link } from "react-router-dom" bunu navbara yaz <Link to="/">Home</Link> yazilabilir 
+
+
 
 function App() {
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+        <Navbar />
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes> */}
+      </Router>
       </header>
-    </div>
+  </div>
   );
 }
 
